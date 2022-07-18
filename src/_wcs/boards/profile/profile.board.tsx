6c876b567a90635c262ea608a49090f58acc5d0a
Dana Levy <danalev@wix.com>
2022-07-18 12:27:59 +0300

    profile props

--- src/_wcs/boards/profile/profile.board.tsx
+++ src/_wcs/boards/profile/profile.board.tsx
@@ -4,7 +4,7 @@ import { Profile } from '../../../profile/profile';
 
 export default createBoard({
     name: 'Profile',
-    Board: () => <Profile />,
+    Board: () => <Profile imageUrl={'https://static.wixstatic.com/media/8586f3_b114d84797ad4818b011c69501c497b1~mv2.png/v1/fill/w_78,h_50,al_c,lg_1,q_85,enc_auto/8586f3_b114d84797ad4818b011c69501c497b1~mv2.png'} />,
     environmentProps: {
         canvasWidth: 340,
         canvasBackgroundColor: '#181717',
--- src/profile/profile.tsx
+++ src/profile/profile.tsx
@@ -4,19 +4,21 @@ import styles from './profile.module.scss';
 export interface ProfileProps {
     className?: string;
     imageUrl: string;
+    name: string;
+    eth: string;
 }
 
-export const Profile: React.FC<ProfileProps> = ({ className, imageUrl }) => {
+export const Profile: React.FC<ProfileProps> = ({ name, eth,className, imageUrl }) => {
     return <div className={`${styles.root} ${className}`}>
         <div className={styles.ProfilePic}>
             <img src={imageUrl} className={styles.image} /></div>
         <div className={styles.CreatorContainer}>
             <p className={styles.Creator}>Creator</p>
-            <p className={styles.CreatorName}>SalvadorDali</p>
+            <p className={styles.CreatorName}>{name}</p>
         </div>
         <div className={styles.BidContainer}>
             <p className={styles.Current}>Current Bid</p>
-            <p className={styles.eth}>4.89 eTH</p>
+            <p className={styles.eth}>{eth}</p>
         </div>
     </div>;
 };
