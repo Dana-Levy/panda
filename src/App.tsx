67617b59057d9575194ba0becea1fb98755597e5
Dana Levy <danalev@wix.com>
2022-07-18 17:13:40 +0300

    card

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styles from './Fonts.module.scss';
-import './_variables.scss'
+
 function App() {
     return (
         <div className="App">
