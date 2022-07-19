a39e82500a3bddbde394779d123aac8b2dc5d3ad
Dana Levy <danalev@wix.com>
2022-07-19 14:49:49 +0300

    card

--- src/_wcs/boards/card/card.board.tsx
+++ src/_wcs/boards/card/card.board.tsx
@@ -4,9 +4,9 @@ import { Card } from '../../../card/card';
 export default createBoard({
     name: 'Card',
     Board: () => <Card title={'Hamlet Contemplates ...'} imageUrl={'https://static.wixstatic.com/media/8586f3_6bc9a9458b9d40b4a0029aef96d5babe~mv2.png/v1/fill/w_470,h_300,al_c,lg_1,q_85,enc_auto/8586f3_6bc9a9458b9d40b4a0029aef96d5babe~mv2.png'} profile={{
-        eth: 'asdasd',
-        imageUrl: 'asdasd',
-        name: 'asdas'
+        eth: '78,098',
+        imageUrl: 'https://cdn.dribbble.com/users/1338391/screenshots/15264109/media/1febee74f57d7d08520ddf66c1ff4c18.jpg?compress=1&resize=400x300&vertical=top',
+        name: 'Alex'
     }} />,
     environmentProps: {
         canvasWidth: 385,
